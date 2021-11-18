bfea3b74a0550e40a58c2cd9de97d6e0268add9a
Degenerate <82691315+Degenerate0001@users.noreply.github.com>
2021-11-18 17:19:04 -0600

    fix weird spacing

--- main.go
+++ main.go
@@ -14,40 +14,40 @@ var (
 func main() {
 
     fmt.Print("\033[H\033[2J") //clears terminal
-	fmt.Print("Would you like to encode (e) or decode (d): ")
-	scanner.Scan()
-	input := scanner.Text()
+    fmt.Print("Would you like to encode (e) or decode (d): ")
+    scanner.Scan()
+    input := scanner.Text()
     
-	if input == "e" {
-		encode()
-	} else if input == "d" {
-		decode()
-	} else {
-		fmt.Println("Dude, that wasn't an option.")
-	}
+    if input == "e" {
+	encode()
+    } else if input == "d" {
+	decode()
+    } else {
+	fmt.Println("Dude, that wasn't an option.")
+    }
 
 }
 
 func encode() {
 
-	fmt.Print("Enter what you would like to be encoded: ")
+    fmt.Print("Enter what you would like to be encoded: ")
     scanner.Scan()
 
-	input := scanner.Text()
-	Enc := base64.StdEncoding.EncodeToString([]byte(input))
+    input := scanner.Text()
+    Enc := base64.StdEncoding.EncodeToString([]byte(input))
 
-	fmt.Println("Your encoded text is:", Enc)
+    fmt.Println("Your encoded text is:", Enc)
 
 }
 
 func decode() {
 
-	fmt.Print("Enter what you would like to be decoded: ")
+    fmt.Print("Enter what you would like to be decoded: ")
     scanner.Scan()
 
-	input := scanner.Text()
-	Dec, _ := base64.StdEncoding.DecodeString(input)
+    input := scanner.Text()
+    Dec, _ := base64.StdEncoding.DecodeString(input)
 
-	fmt.Println("Your decoded text is:", (string(Dec)))
+    fmt.Println("Your decoded text is:", (string(Dec)))
 	
 }
